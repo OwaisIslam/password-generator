d09a79ad000547fa14675fa08a41c810aa9ebf45
Owais Islam <owaisislam@gmail.com>
2020-08-14 16:18:31 -0500

    added more user choices and passwordLength validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,22 @@ function generatePassword() {
   console.log(lowerCase);
   var upperCase = getUpperCase();
   console.log(upperCase);
+  var numericCharacters = getNumeric();
+  console.log(numericCharacters);
+  var specialCharacters = getSpecial();
+  console.log(specialCharacters);
 }
 
 function getPasswordLength() {
   var userChoice = 0;
   while ((userChoice < 8) || (userChoice > 128)) {
     userChoice = parseInt(window.prompt("Enter the number of characters between 8 and 128: "));
+    
+    // checking here to make sure the user entered a number and not a letter
+    if (isNaN(userChoice)) {
+      // this will reset the choice value to 0 so it can restart the loop if the user entered anything besides a number
+      userChoice = 0;
+    }
   }
   return userChoice;
 }
@@ -33,7 +43,33 @@ function getLowerCase() {
 function getUpperCase() {
   var userChoice = "a";
   while (userChoice = "a") {
-    userChoice = (window.prompt("Would you like lowercase characters (y/n)?"));
+    userChoice = (window.prompt("Would you like uppercase characters (y/n)?"));
+    console.log(userChoice);
+    if (userChoice == "y") {
+      return true;
+    } else if (userChoice == "n") {
+      return false;
+    }
+  }
+}
+
+function getNumeric() {
+  var userChoice = "a";
+  while (userChoice = "a") {
+    userChoice = (window.prompt("Would you like numeric characters (y/n)?"));
+    console.log(userChoice);
+    if (userChoice == "y") {
+      return true;
+    } else if (userChoice == "n") {
+      return false;
+    }
+  }
+}
+
+function getSpecial() {
+  var userChoice = "a";
+  while (userChoice = "a") {
+    userChoice = (window.prompt("Would you like special characters (y/n)?"));
     console.log(userChoice);
     if (userChoice == "y") {
       return true;
