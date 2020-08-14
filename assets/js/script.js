dd7190518c3eed521e4b8c361aef6b7a98d671fa
Owais Islam <owaisislam@gmail.com>
2020-08-13 22:58:53 -0500

    added passwordLength function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 // Assignment code here
 
+function generatePassword() {
+  var passwordLength = getPasswordLength();
+  console.log(passwordLength);
+}
+
+function getPasswordLength() {
+  var userLengthChoice = 0;
+  while ((userLengthChoice < 8) || (userLengthChoice > 128)) {
+    userLengthChoice = parseInt(window.prompt("Enter the number of characters between 8 and 128: "));
+    console.log(userLengthChoice);
+  }
+  return userLengthChoice;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +23,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
