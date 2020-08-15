ace8f1d10d40df7500ed710e8a9f7f42e8eb40c4
Owais Islam <owaisislam@gmail.com>
2020-08-15 17:27:23 -0500

    updated special characters list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ function generatePassword() {
   var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}"];
+  var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
   var selectedArray = [];
 
   var passwordLength = getPasswordLength();
