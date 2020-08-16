dab367d99659ea18915b7cc0eef2847879f11811
Owais Islam <owaisislam@gmail.com>
2020-08-16 15:15:30 -0500

    updated for ease of usability on mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,6 +71,8 @@ function getChoice(currentOption) {
   // This loop ensures the user enters a valid response.
   while (userChoice = "a") {
     userChoice = (window.prompt(messagePrompt));
+    // Added the line below for ease of usability on mobile devices as some of them automatically capitalize when entering input.
+    userChoice = userChoice.toLowerCase();
     if (userChoice == "y") {
       return true;
     } else if (userChoice == "n") {
